42cea01b5f15497869a8b5182df0436f8529b61c
Madhumitha R <98893549+madhumitha-r26@users.noreply.github.com>
2024-09-04 09:00:55 +0000

    committed

--- index.js
+++ index.js
@@ -1,23 +1,58 @@
 const express = require("express");
 const {users}= require("./data/users.json");
+const {books}= require("./data/books.json");
 const app = express();
 const PORT = 8081;
 
 app.use(express.json());
 
-app.get("/users",(req,res) =>{
-    res.status(200).json({
-        success:true,
-        data: users,
-    });
-});
+/*
++ route: /users
++ method: GET
++ description: get all users
++ access: public
++ parameters: none
+*/
 
+
+
+/*
++ route: /users/:id
++ method: GET
++ description: get single user by their id
++ access: public
++ parameters: id
+*/
+
+app.get("/users/:id",(res,req)=>{
+    const {id} = req.params;
+    const user= users.find((each)=> each.id ===id);
+    if(user){
+        return res.status(404).json({
+            success: false,
+            message:"user doesn't exist"
+        });
+    }
+    else{
+        app.get("/users",(req,res) =>{
+            res.status(200).json({
+                success:true,
+                data: users,
+            });
+        });
+    }
+});
 
 
 app.get("/", (req, res) => {
     res.status(200).json({
     message: "Server is up and running :-))",
     data:"hey",
+});
+return res.status(404).json({
+    success: true,
+    message:"user found",
+    data:user
 });
 }); 
 
